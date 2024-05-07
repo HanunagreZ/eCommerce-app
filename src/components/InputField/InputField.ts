e058b9dc2c68ac989cee4e08abb299bd422f9812
EugeniaRe <evrepni@gmail.com>
2024-05-08 01:24:38 +0500

    EA-45 feat: add callback to InputField

--- src/components/InputField/InputField.ts
+++ src/components/InputField/InputField.ts
@@ -9,10 +9,11 @@ export default class InputField {
   private input: Input;
   private clue: Div;
 
-  constructor(parentElement: HTMLElement, labelText: string, clueText: string) {
+  constructor(parentElement: HTMLElement, labelText: string, clueText: string, callback: () => void) {
     this.container = new Div('input-field', parentElement);
     this.label = new Label(labelText, 'input-field-label', this.container.get());
     this.input = new Input('', 'input-field-input', this.container.get());
+    this.input.addListener(callback);
     this.clue = new Div('input-field-clue', this.container.get());
     this.clue.get().innerText = clueText;
   }
