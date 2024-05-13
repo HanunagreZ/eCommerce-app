debea2a93f5619ce9dc91c2c1cf2dd8aa8404905
Eugeniya <evrepni@gmail.com>
2024-05-14 02:24:09 +0500

    EA-45 fix: clean index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
 import './styles.scss';
-import Registration from './Pages/Registration/Registration';
-// import Login from './Pages/Login/Login';
-// import { generalData } from './constants/registration';
-
-// const registration = new Registration(document.body);
-
-//registration.fillForm(generalData);
-document.body.append(new Registration().render());
-
-// new Login(document.body);
