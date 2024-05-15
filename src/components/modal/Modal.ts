8b6a1fd2f7416fcca3a5ba1f46988229705d0472
HanunagreZ <hanunagrez@yandex.ru>
2024-05-15 10:12:16 +0300

    EA-58 refactor: rewrote the logic of modal windows, added an object with data for them

--- src/components/modal/Modal.ts
+++ src/components/modal/Modal.ts
@@ -2,28 +2,34 @@ import Button from '../../ui-components/Button/Button';
 import Span from '../../ui-components/Span/Span';
 import Div from '../../ui-components/Div/Div';
 import './modal.scss';
+import { IModalProps } from '../../interfaces/interfaces';
 
-export default class ModalServerError {
+export default class Modal {
   private container: Div;
   private background: Div;
+  private props: IModalProps;
+  private parentElement: HTMLElement;
 
-  constructor(className: string, parentElement: HTMLElement) {
-    this.container = new Div(className, parentElement);
+  constructor(props: IModalProps) {
+    this.parentElement = document.body;
+    this.container = new Div('modal', this.parentElement);
     this.background = new Div('modal__background', this.container.get());
-    this.render(parentElement);
+    this.props = props;
+    this.render(this.parentElement);
   }
 
   render(parentElement: HTMLElement) {
     const icon = document.createElement('img');
     icon.classList.add('modal__icon');
-    icon.src = 'assets/icons/iconAlert.svg';
-    const title = new Span('Something went wrong', 'modal__title', this.container.get());
-    const description = new Span('Please try again', 'modal__description', this.container.get());
-    const btn = new Button('Back', 'modal__btn', this.container.get());
+    icon.src = this.props.icon;
+    const title = new Span(this.props.title, 'modal__title', this.container.get());
+    const description = new Span(this.props.description, 'modal__description', this.container.get());
+    const btn = new Button(this.props.btn, 'modal__btn', this.container.get());
 
     btn.get().addEventListener('click', () => {
       this.container.get().remove();
       this.background.get().remove();
+      this.props.addEvent();
     });
 
     this.container.get().append(icon, title.get(), description.get(), btn.get());
