ebc78cfb10acf759b528dbb58d976372aa45aac0
vwital <86975341+vwital@users.noreply.github.com>
2024-05-16 08:49:57 +0300

    EA-91 (404 Page) (#11)
    * EA-91 feat: add 404 page markup
    
    * EA-91 feat: add 404 styles
    
    * EA-91 refactor: change style properties order according to the rules delete unnecessary code
    
    * EA-91 fix: edit font family feat: adaptive layout

--- src/pages/404/404.ts
+++ src/pages/404/404.ts
@@ -1,10 +1,31 @@
-export default class Page404 {
-  render() {
-    const elem = document.createElement('div');
-    const header = document.createElement('h2');
-    header.textContent = '404. Page Not Found';
-    elem.appendChild(header);
+import './404.scss';
+import Div from '../../ui-components/Div/Div';
+import Button from '../../ui-components/Button/Button';
 
-    return elem;
+export default class Page404 {
+  private container: Div;
+  private pageHeader: HTMLHeadingElement;
+  private pageSubHeader: HTMLHeadingElement;
+  private pageInfo: HTMLParagraphElement;
+  private button: Button;
+
+  constructor() {
+    this.container = new Div('wrapper__404');
+    this.pageHeader = document.createElement('h2');
+    this.pageHeader.classList.add('title__404');
+    this.pageHeader.textContent = '404';
+    this.pageSubHeader = document.createElement('h3');
+    this.pageSubHeader.classList.add('description__404');
+    this.pageSubHeader.textContent = 'Page not found';
+    this.pageInfo = document.createElement('p');
+    this.pageInfo.classList.add('info__404');
+    this.pageInfo.textContent = 'Oops!, the page you looking for does not exist';
+    this.button = new Button('Return to main', 'button__404');
+    this.button.get().onclick = () => (window.location.href = '/');
+  }
+
+  render() {
+    this.container.get().append(this.pageHeader, this.pageSubHeader, this.pageInfo, this.button.get());
+    return this.container.get();
   }
 }
