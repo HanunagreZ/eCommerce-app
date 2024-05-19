171d28f5157c97b9262795d929bdacf164773b36
Eugeniya <evrepni@gmail.com>
2024-05-19 23:55:41 +0500

    EA-39; EA-45 fix: delete function gitCookie from utils

--- src/utils/getCookie.ts
+++
@@ -1,4 +0,0 @@
-export function getCookie(name: string): string | null {
-  const cookie = document.cookie.split('; ').find((el) => el.startsWith(name + '='));
-  return cookie ? cookie.split('=')[1] : null;
-}
