550e3f7d4b34d2781626efba84d0cc865b46e66a
Eugeniya <evrepni@gmail.com>
2024-05-23 03:04:56 +0500

    EA-59 feat: factory for creating products cards

--- src/Pages/Catalog/Product.ts
+++ src/Pages/Catalog/Product.ts
@@ -1,6 +1,19 @@
-class Product{
+import { IProductCard } from '../../interfaces/interfaces';
+import Button from '../../ui-components/Button/Button';
+import Div from '../../ui-components/Div/Div';
+import Img from '../../ui-components/Img/Img';
+import Span from '../../ui-components/Span/Span';
 
-    constructor(imgSrc: string, name: string, price: string){
+export class Product {
+  constructor(productData: IProductCard) {
+    this.render(productData);
+  }
 
-    }
+  render(productData: IProductCard) {
+    const card = new Div('catalog__prooduct-card');
+    new Img('catalog__card-img', productData.imgSrc, 'product_image', card.get());
+    new Span(productData.name, 'catalog__product-name', card.get());
+    new Span(productData.price, 'catalog__product-price', card.get());
+    const button = new Button('Add to cart', 'catalog__card-btn', card.get());
+  }
 }
