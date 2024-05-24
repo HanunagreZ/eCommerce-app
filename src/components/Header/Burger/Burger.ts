9b4aa9349f121d7333d3768b46695a4ede103906
Aleksey <94462853+HanunagreZ@users.noreply.github.com>
2024-05-24 09:47:43 +0300

    EA-89 (Header) (#20)
    * EA-89 feat: add new hover effect for header links
    
    * EA-89 feat: add dropdown menu on catalog link

--- src/components/Header/Burger/Burger.ts
+++ src/components/Header/Burger/Burger.ts
@@ -5,6 +5,7 @@ import userState from '../../../states/UserState';
 
 class Burger {
   private element: HTMLDivElement;
+  private divBurgerLines: Div;
   private burgerLine1: HTMLImageElement;
   private burgerLine2: HTMLImageElement;
   private burgerLine3: HTMLImageElement;
@@ -14,6 +15,7 @@ class Burger {
 
   constructor() {
     this.element = document.createElement('div');
+    this.divBurgerLines = new Div('header__divBurgerLines', this.element);
     this.element.classList.add('header__burger');
     this.burgerLine1 = document.createElement('img');
     this.burgerLine1.src = 'assets/icons/burgerLine.svg';
@@ -25,7 +27,6 @@ class Burger {
     this.burgerLine3.src = 'assets/icons/burgerLine.svg';
     this.burgerLine3.classList.add('header__burger-line3');
     this.popup = new Div('burger__popup', this.element);
-    this.clickHamburger();
     this.navPopupDiv = new Div('header__nav_popupDiv', this.popup.get());
     this.navUnauthPopup = new NavUnauth('header__nav_popup');
   }
@@ -35,22 +36,36 @@ class Burger {
   }
 
   clickHamburger() {
-    this.element.addEventListener('click', () => {
-      this.burgerLine1.classList.toggle('header__burger-line1_active');
-      this.burgerLine2.classList.toggle('header__burger-line2_active');
-      this.burgerLine3.classList.toggle('header__burger-line3_active');
-      this.popup.get().classList.toggle('burger__popup_active');
-    });
+    this.burgerLine1.classList.toggle('header__burger-line1_active');
+    this.burgerLine2.classList.toggle('header__burger-line2_active');
+    this.burgerLine3.classList.toggle('header__burger-line3_active');
+    this.popup.get().classList.toggle('burger__popup_active');
   }
 
   render(parentElement: HTMLElement) {
-    this.element.append(this.burgerLine1, this.burgerLine2, this.burgerLine3);
+    this.divBurgerLines.get().addEventListener('click', () => {
+      this.clickHamburger();
+    });
+
+    this.navUnauthPopup.get().addEventListener('click', (e) => {
+      if (e.target !== this.navUnauthPopup.get()) {
+        this.clickHamburger();
+      }
+    });
+
+    this.divBurgerLines.get().append(this.burgerLine1, this.burgerLine2, this.burgerLine3);
     parentElement.append(this.element);
   }
   renderPopupNav() {
     if (userState.getUserName()) {
       const navAuthPopup = new NavAuth('header__nav_popup');
       this.popup.get().children[0].replaceWith(navAuthPopup.get());
+
+      navAuthPopup.get().addEventListener('click', (e) => {
+        if (e.target !== this.navUnauthPopup.get()) {
+          this.clickHamburger();
+        }
+      });
     } else {
       this.popup.get().children[0].replaceWith(this.navUnauthPopup.get());
     }
